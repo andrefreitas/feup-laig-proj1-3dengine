cb8cb81405c3cc57b5951dbef3577658fa15183e
paulo <paulojmaraujo@gmail.com>
2012-09-27 13:20:51 +0100

    adicionados cabeçalhos de funções

--- code/include/lsfParser.h
+++ code/include/lsfParser.h
@@ -7,11 +7,23 @@
 
 class lsfParser:{
 	private:
+		TiXmlDocument* file;
+		
+		TiXmlNode* node;
+		
+		TiXmlElement* globals;
+		TiXmlElement* cameras;
+		TiXmlElement* lighting;
+		TiXmlElement* appearances;
+		TiXmlElement* graph;
 		
 	public:
-		void readGlobasl();
-		void readCameras();
-		void readLighting();
-		void readAppearances();
-		void readGraph();
+		void loadFile(TiXmlDocument* file);
+		void readGlobals(TiXmlElement* globals);
+		void readCameras(TiXmlElement* cameras);
+		void readLighting(TiXmlElement* lighting);
+		void readAppearances(TiXmlElement* appearances);
+		void readGraph(TiXmlElement* graph);
+		
+		
 }
