716209324131ec4c9be71f59c805cc7d6f6942d6
Paulo Araújo <paulojmaraujo@gmail.com>
2012-09-28 01:45:53 +0100

    modificação do load file para código genérico

--- code/src/lsfParser.cpp
+++ code/src/lsfParser.cpp
@@ -7,9 +7,9 @@ lsfParser::lsfParser(char* inputFile){
 	file = new TiXmlDocument(inputFile);
 
 	if(file->LoadFile()){
-		std::cout << "File ring.lsf is load" << std::endl;
+		std::cout << "File " << inputFile << " is load" << std::endl;
 	}
 	else{
-		std::cout << "Error openning file ring.lsf" << std::endl;
+		std::cout << "Error openning file " << inputFile << std::endl;
 	}
 }
